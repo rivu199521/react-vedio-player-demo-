87cafdc67c980585c1a380f392ba37949aeceaac
unknown <mukherjeerivu7@gmail.com>
2020-07-29 08:26:47 +0530

    Updates

---
+++ precache-manifest.86172ed413149b25de41fea261816f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024dc09a4b95419dbda9d57ec70aeac2",
+    "url": "/react-vedio-player-demo/index.html"
+  },
+  {
+    "revision": "2ec1a3968cf3fae14590",
+    "url": "/react-vedio-player-demo/static/css/main.b7f521ac.chunk.css"
+  },
+  {
+    "revision": "f2e58e8e96c4c33431a2",
+    "url": "/react-vedio-player-demo/static/js/2.99f4cc10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-vedio-player-demo/static/js/2.99f4cc10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec1a3968cf3fae14590",
+    "url": "/react-vedio-player-demo/static/js/main.fc725faa.chunk.js"
+  },
+  {
+    "revision": "b32c0385a022b5449bf6",
+    "url": "/react-vedio-player-demo/static/js/runtime-main.119a892b.js"
+  }
+]);
