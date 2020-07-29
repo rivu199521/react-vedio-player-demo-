296b266307102b38207601dbb461f5b02cb46f5d
unknown <mukherjeerivu7@gmail.com>
2020-07-29 08:22:49 +0530

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactPlayer from 'react-player'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactPlayer url='https://www.youtube.com/watch?v=oYDnmDNdSIA'
+      controls
+
+      onReady={() => console.log('onReady callback')}
+      onStart={() => console.log('onStart callback')}
+      onPause={() => console.log('onPause callback')}
+      onError={() => console.log('onError callback')}
+      
+      />
     </div>
   );
 }
